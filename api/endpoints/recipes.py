10dc287de500bbc44d07057bd65f7671b40f69dc
Max Li <143444069+notttired@users.noreply.github.com>
2025-05-09 02:17:18 -0400

    Added endpoint for basic functionality

--- api/endpoints/recipes.py
+++ api/endpoints/recipes.py
@@ -4,6 +4,7 @@ from scraper.recipe_scraper import RecipeScraper
 from database.database_service import DatabaseService
 from models.recipe import Recipe
 from services.ai_api_service import AIAPIService
+from typing import Optional
 
 app = FastAPI()
 
@@ -17,10 +18,9 @@ ai_api_service = AIAPIService()
 def suggest_recipes(urls: list[str]) -> None:
     "Adds new recipes to the database by scraping the provided URLs."
     recipe_service.suggest_recipes(urls)
-    return {"message": "Recipes suggested successfully"}
 
 @app.post("/recipes/filter")
-def get_recipes(intent: str) -> list[Recipe]:
+def get_recipes(intent: str) -> Optional[list[Recipe]]:
     recipe_filters = ai_api_service.get_tags(intent)
     recipes = recipe_service.get_recipes(recipe_filters)
     return recipes
